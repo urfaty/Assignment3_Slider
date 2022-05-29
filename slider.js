aef3d2aff83694fd62cf3bfd0315593daef24554
urfaty <97344694+urfaty@users.noreply.github.com>
2022-05-29 21:26:29 +0300

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+        	$("#slider").bxSlider({
+				auto: true,
+				minSlides: 1,
+				maxSlides: 1,
+				slideWidth: 250,
+				slideMargin: 10,
+				randomStart: true,
+				moveSlides: 1,
+				speed: 3000,
+				pager: true,
+				pagerType: "short",
+				pagerSelector: "#pager",
+				captions: true
+
+
+
+			  
+			});
+    	});
